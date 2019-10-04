43286ff370c40eb4928ec5e8cb55bcc8bdf638b4
Matthew Jones <matts3371@gmail.com>
2019-10-04 02:14:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
   comp_num = rand(1..6)
-  while user_input != "exit" do
+  if user_input != "exit" 
     # Compare input to random num
     # Print result: correct?
     if user_input.to_i == comp_num
